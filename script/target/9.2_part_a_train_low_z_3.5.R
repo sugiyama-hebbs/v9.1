f011e320b3eaeb686660302c5a79e92b4facfa51
Takashi Ito <ito@hebbs.emp.tsukuba.ac.jp>
2021-05-31 11:30:03 +0900

    added version 9.2 (preliminary for testing the experiment programs, it will be edited further soon)

--- script/target/9.2_part_a_train_low_z_3.5.R
+++ script/target/9.2_part_a_train_low_z_3.5.R
@@ -12,7 +12,7 @@ set.seed(2) # Set a fix seed so that a sequence can be replicated
 # format: sprintf("script/target/output/%d%s_%s%s_%s.txt",blk_tag,pre_tag,cond,pos_tag,version_id)
 version_id = "9.2" # version id
 blk_tags <- 0 # block tag
-cond <- "train_lrn_mid_z" # condition. Also the main part of filename
+cond <- "train_low_z_3.5" # condition. Also the main part of filename
 pre_tag <- ""
 pos_tag <- ""
 
@@ -20,16 +20,13 @@ pos_tag <- ""
 source("script/target/subscript/set_taskwide_param.R") # set task-wide parameters
 
 rot_size <- 3.5 # visual rotation size (degree)
-tpc <- spc_te + mpc_te # trials per cycle
-
-cps <- 20 # cycles per one set 
-spb <- 3 # sets per block
+tpc <- 7# spc_te + mpc_te # trials per cycle
 
 ph0 = 0 # trials in phase 0 (Familiarization/Pre-training)
 ph1 = 0 # trials in phase 1 (initial washouts in main blocks)
 ph2 = 0 # trials in phase 2 (Pre-Probe)
 ph3 = 0 # trials in phase 3 (washouts before Train)
-ph4 = tpc*cps # trials in phase 4 (Train)
+ph4 = tpc*cpb_9.2 # cycles per block # trials in phase 4 (Train)
 ph5 = 0 # trials in phase 5 (Post-Train Washout)
 ph6 = 0 # trials in phase 6 (Post-Probe)
 ph7 = 0 # trials in phase 7 (visuomotor)
@@ -41,13 +38,14 @@ num_tri <- ph0 + ph1 + ph2 + ph3 + ph4 + ph5 + ph6 + ph7 + ph8 + ph9
 
 min_score <- rep(-20,num_tri)  
 max_score <- rep(0,num_tri) 
-difficulty <- rep(1,num_tri)
+difficulty <- rep(0.5,num_tri)
 
-train_type <- rep(1,num_tri) # This will be replaced, so put some number that is easy to notice in case replacement is not done.
+train_type_lrn <- rep(1,num_tri) 
+train_type_nlrn <- rep(2,num_tri) 
 
-s_tri <- rep(c(rep(1,spc_te),rep(0,mpc_te)),cps) # flag s trial
-m_tri <- rep(c(rep(0,spc_te),rep(1,mpc_te)),cps) # flag m trial
-
+s_tri <- rep(c(1,0,rep(1,5)),cpb_9.2) # flag s trial
+m_tri <- rep(c(0,1,rep(0,5)),cpb_9.2) # flag m trial
+s_tri_rot <- rep(c(1,rep(0,6)),cpb_9.2)
 
 ## Rotation pattern in Train. Hard coding to mimic the sequence in the original version
 # rot_pattern <- c(-1,1,1,-1,1,1,-1,-1,-1,
@@ -56,7 +54,8 @@ m_tri <- rep(c(rep(0,spc_te),rep(1,mpc_te)),cps) # flag m trial
 #                  1,-1
 # )
 
-rot_pattern <- rep(c(-1,1,1,-1,-1,1,-1,-1,1,1),cpb_te/10) # z ~ 0.1
+# rot_pattern <- rep(c(rep(-1,4),rep(1,3),rep(-1,3),rep(1,4),rep(-1,3),rep(1,3)),cpb_te/20) # z ~ 0.1
+rot_pattern <- rep(c(-1,1,-1,1,-1,1,1,-1,-1,1),cpb_9.2/10) # z ~ 0.1
 # now changed the pattern to avoid the same rotation 5 times 
 
 # initialize
@@ -67,13 +66,16 @@ rot_degree <- rep(0,num_tri)
 show_score <- rep(0,num_tri)
 
 # assign appropriate values to each type of trial
-rot_degree[s_tri == 1] <- rot_size*rot_pattern
+rot_degree[s_tri_rot == 1] <- rot_size*rot_pattern
 show_arc[s_tri == 1] <- 4
 show_arc[m_tri == 1] <- 5
 show_cur[s_tri == 1] <- 2
 trial_type[s_tri == 1] <- 2
 trial_type[m_tri == 1] <- 3
 show_score[m_tri == 1] <- 1
+# show_score[m_tri == 1] <- 0
+
+
 
 tmod_v <- seq(-15,15,3) # set of modification values on target direction
 num_tmod <- length(tmod_v) # number of modifying direction
@@ -98,31 +100,40 @@ if (num_tri%%num_tmod != 0){
 t_deg_raw <- rep(t_deg_ref, num_tri)
 t_deg <-  rep(t_deg_ref, num_tri) + t_deg_mod
 
-## Break time
-break_time <- rep(0,num_tri)
-break_time[num_tri] <- 30 # break at the end of set
-
 ### These parameters should be the same across the task, so no need to edit in each script
 field <-  rep(0,num_tri)  # No FF in this task
 apply_field <-  rep(0,num_tri)
 t_radius <- rep(t_radius_ref, num_tri)
-wait_time <-  (wait_min + ceiling(runif(num_tri,0,addwait_max)))/1000 # Wait time
+wait_time <-  (wait_min_mod + ceiling(runif(num_tri,0,addwait_max_mod)))/1000 # Wait time
 bval <- rep(0,num_tri)  # no FF, so 0 bvalue
 channel_k11 <- rep(0,num_tri)  # no FF, so 0 bvalue
 channel_b11 <- rep(0,num_tri)  # no FF, so 0 bvalue
 gain <- rep(0,num_tri)  # no FF, so 0 bvalue
-
-
-
+task_break <- rep(0,num_tri) # temporarily
 
 # combine
-seq.tgt <- cbind(field, apply_field, t_radius, t_deg, wait_time, 
+
+seq.tgt_lrn <- cbind(field, apply_field, t_radius, t_deg, wait_time, 
                  bval, channel_k11, channel_b11, gain, rot_degree,
-                 show_arc, show_cur, show_score, train_type, min_score,
-                 max_score, difficulty, trial_type, blk_phase,break_time)
+                 show_arc, show_cur, show_score, train_type_lrn, min_score,
+                 max_score, difficulty, trial_type, blk_phase, task_break)
+
+seq.tgt_nlrn <- cbind(field, apply_field, t_radius, t_deg, wait_time, 
+                     bval, channel_k11, channel_b11, gain, rot_degree,
+                     show_arc, show_cur, show_score, train_type_nlrn, min_score,
+                     max_score, difficulty, trial_type, blk_phase, task_break)
+
+seq.tgt_base <- cbind(field, apply_field, t_radius, t_deg, wait_time,
+                      bval, channel_k11, channel_b11, gain, rot_degree,
+                      show_arc, show_cur, rep(0,num_tri), rep(0,num_tri), rep(0,num_tri),
+                      rep(0,num_tri), difficulty, trial_type, blk_phase, task_break)
 
 for (blk_tag in blk_tags){
-  write.table(seq.tgt,sprintf("script/target/output/part/%d%s_%s%s_%s.txt",blk_tag,pre_tag,cond,pos_tag,version_id),
+  write.table(seq.tgt_lrn,sprintf("script/target/output/part/%d%s_%s%s_lrn_%s.txt",blk_tag,pre_tag,cond,pos_tag,version_id),
+              row.names = F, col.names = F, sep = " ")
+  write.table(seq.tgt_nlrn,sprintf("script/target/output/part/%d%s_%s%s_nlrn_%s.txt",blk_tag,pre_tag,cond,pos_tag,version_id),
+              row.names = F, col.names = F, sep = " ")
+  write.table(seq.tgt_base,sprintf("script/target/output/part/%d%s_%s%s_baseline_%s.txt",blk_tag,pre_tag,cond,pos_tag,version_id),
               row.names = F, col.names = F, sep = " ")
 }
 
--- script/target/subscript/set_taskwide_param.R
+++ script/target/subscript/set_taskwide_param.R
@@ -19,3 +19,4 @@ cpb_te <- 60 # cycles per block
 cpb_te_vary_rot <- 16 # cycles per block
 cpb_te_short <- 30 # cycles per block
 cpb_te_9.14 <- 10 # cycles per block
+cpb_9.2 <- 10 # cycles per block
