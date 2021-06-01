b7f23edc2f2c9870a23521ea52edaba368cdc96a
sugiyama-hebbs <82080354+sugiyama-hebbs@users.noreply.github.com>
2021-06-01 11:18:08 +0900

    bug fix on read.csv

--- script/target/9.2_make_train_blk.R
+++ script/target/9.2_make_train_blk.R
@@ -1,9 +1,9 @@
 # make a train block from parts
 
 
-seq_probe <- read.csv("script/target/output/part/0_probe_part_9.2.txt", header = F)
-seq_lrn <- read.csv("script/target/output/part/0_train_low_z_3.5_lrn_9.2.txt", header = F)
-seq_nlrn <- read.csv("script/target/output/part/0_train_low_z_3.5_nlrn_9.2.txt", header = F)
+seq_probe <- read.csv("script/target/output/part/0_probe_part_9.2.txt", header = F, sep=" ")
+seq_lrn <- read.csv("script/target/output/part/0_train_low_z_3.5_lrn_9.2.txt", header = F, sep=" ")
+seq_nlrn <- read.csv("script/target/output/part/0_train_low_z_3.5_nlrn_9.2.txt", header = F, sep=" ")
 
 seq.tgt_lrn <- rbind(seq_probe,seq_lrn,seq_probe,seq_lrn,seq_probe,seq_lrn,seq_probe)
 seq.tgt_nlrn <- rbind(seq_probe,seq_nlrn,seq_probe,seq_nlrn,seq_probe,seq_nlrn,seq_probe)
